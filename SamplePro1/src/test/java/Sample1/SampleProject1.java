0f2487eb583d446a10a300f2e25f20344b1b51fa
Mohan S <kingsleydavid.s@thestreet.com>
2018-11-25 22:39:54 +0530

    hi new

---
+++ SamplePro1/src/test/java/Sample1/SampleProject1.java
@@ -0,0 +1,15 @@
+package Sample1;
+
+import org.testng.annotations.Test;
+
+public class SampleProject1 {
+	
+	
+	@Test
+	public void sampleProject(){
+		System.out.println("Hi Kingsley, How are you");
+		System.out.println("I am fine, by the grace of God");
+		
+	}
+
+}
